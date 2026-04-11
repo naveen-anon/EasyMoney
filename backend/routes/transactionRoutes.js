88a99350b20f655ccf4b7dd49f793d818f2fbe95
Naveen khatri <naveen1khatri@gmail.com>
2026-04-11 15:48:06 +0530

    Add transaction routes for deposit and withdraw

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -1,1 +1,11 @@
 
+const express = require('express');
+const auth = require('../middlewares/authMiddleware');
+const { deposit, withdraw } = require('../controllers/transactionController');
+
+const router = express.Router();
+
+router.post('/deposit', auth, deposit);
+router.post('/withdraw', auth, withdraw);
+
+module.exports = router;
