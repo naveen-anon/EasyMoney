973805ab7c15cd57b72df04dd092b4555b9a9c49
Naveen khatri <naveen1khatri@gmail.com>
2026-04-11 12:47:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
 
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require('./routes/auth');
+const depositRoutes = require('./routes/deposit');
+
+app.use('/api/auth', authRoutes);
+app.use('/api/deposit', depositRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API Running 🚀');
+});
+
+app.listen(5000, () => console.log("Server running"));
