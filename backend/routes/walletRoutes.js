aabbc368e6b87194b058eaa6f0c1e133faf7adc6
Naveen khatri <naveen1khatri@gmail.com>
2026-04-11 15:47:02 +0530

    Update walletRoutes.js

--- backend/routes/walletRoutes.js
+++ backend/routes/walletRoutes.js
@@ -1,1 +1,10 @@
 
+const express = require('express');
+const auth = require('../middlewares/authMiddleware');
+const { getBalance } = require('../controllers/walletController');
+
+const router = express.Router();
+
+router.get('/', auth, getBalance);
+
+module.exports = router;
